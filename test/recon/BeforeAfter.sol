7f45028777b48cdcba45f2e136e110788220829b
Simon Busch <Simon.busch@live.fr>
2024-12-07 08:41:22 +0100

    test

--- test/recon/BeforeAfter.sol
+++ test/recon/BeforeAfter.sol
@@ -17,6 +17,10 @@ abstract contract BeforeAfter is Setup {
         _before.counter_number = counter.number();
     }
 
+
+
+
+
     function __after() internal {
         _after.counter_number = counter.number();
     }
